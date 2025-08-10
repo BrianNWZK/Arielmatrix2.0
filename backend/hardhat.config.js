76d98d148620f766fcd2d56d8f76b98e40d53c72
BrianNWZK <brianwaezike@gmail.com>
2025-08-10 16:31:03 +0100

    Update hardhat.config.js

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,7 +1,7 @@
 import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
 
-const config = {
+const config: HardhatUserConfig = {
   solidity: {
     version: '0.8.20',
     settings: {
