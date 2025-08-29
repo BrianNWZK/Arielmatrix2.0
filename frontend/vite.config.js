6e53f15ec19d9914331e8f522928d60305425e26
BrianNWZK <brianwaezike@gmail.com>
2025-08-29 11:32:09 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: 'frontend', // Set the root to the frontend directory
+  root: './frontend', // Set the root to the frontend directory
   publicDir: 'public',
   build: {
     outDir: 'dist',
