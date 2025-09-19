b508100c360f884cdf8148eaa4fc044ab1d5c1b4
BrianNWZK <brianwaezike@gmail.com>
2025-09-19 13:47:54 +0100

    Update deployBwaeziContract.js

--- scripts/deployBwaeziContract.js
+++ scripts/deployBwaeziContract.js
@@ -1,4 +1,8 @@
 const { ethers } = require("hardhat");
+const totalSupply = BWAEZI_SOVEREIGN_CONFIG.TOTAL_SUPPLY;
+const founder = BWAEZI_SOVEREIGN_CONFIG.SOVEREIGN_OWNER;
+
+await contract.deploy({ totalSupply, founder });
 
 async function main() {
     const BwaeziCore = await ethers.getContractFactory("BwaeziCore");
