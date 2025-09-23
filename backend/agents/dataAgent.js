ff142b69e20634fe90b0bd58d792a0200c258f8e
BrianNWZK <brianwaezike@gmail.com>
2025-09-23 12:15:51 +0100

    Update dataAgent.js

--- backend/agents/dataAgent.js
+++ backend/agents/dataAgent.js
@@ -3,7 +3,7 @@ import crypto from 'crypto';
 import { createDatabase } from '../../modules/your-sqlite/index.js'; 
 import QuantumBrowserManager from './browserManager.js';
 import { BrianNwaezikeChain } from '../blockchain/BrianNwaezikeChain.js';
-import { Connection, PublicKey, LAMPORTS_PER_SOL, Keypair, Transaction, SystemProgram, sendAndConfirmTransaction } from '@solana/spl-token';
+import { Connection, PublicKey, LAMPORTS_PER_SOL, Keypair, Transaction, SystemProgram, sendAndConfirmTransaction } from '@solana/web3.js';
 import { getAssociatedTokenAddress, createTransferInstruction } from '@solana/spl-token';
 import apiScoutAgent from './apiScoutAgent.js';
 import {
