29fa5ca78ff87b2ed350b1bb417ac8a7206ee19e
BrianNWZK <brianwaezike@gmail.com>
2025-09-23 13:28:21 +0100

    Update dataAgent.js

--- backend/agents/dataAgent.js
+++ backend/agents/dataAgent.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import crypto from 'crypto';
-import { createDatabase } from '../../modules/your-sqlite/index.js'; 
+import { ArielSQLiteEngine } from '../../modules/your-sqlite/index.js'; 
 import { QuantumBrowserManager } from './browserManager.js';
 import { BrianNwaezikeChain } from '../blockchain/BrianNwaezikeChain.js';
 import { Connection, PublicKey, LAMPORTS_PER_SOL, Keypair, Transaction, SystemProgram, sendAndConfirmTransaction } from '@solana/web3.js';
