8a274878c32503c6971ca2de89432f7330669393
BrianNWZK <brianwaezike@gmail.com>
2025-09-26 16:11:12 +0100

    Update configAgent.js

--- backend/agents/configAgent.js
+++ backend/agents/configAgent.js
@@ -6,7 +6,7 @@ import { dataAgent } from './dataAgent.js';
 import { adsenseAgent } from './adsenseAgent.js';
 import { adRevenueAgent } from './adRevenueAgent.js';
 import { autonomousAIEngine } from './autonomous-ai-engine.js';
-import { serviceManager } from '../../serviceManager.js';
+import { serviceManager } from '../../arielsql_suite/serviceManager.js';
 
 export const configAgent = async (CONFIG) => {
   try {
