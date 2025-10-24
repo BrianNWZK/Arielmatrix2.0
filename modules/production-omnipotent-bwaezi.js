d748efcd04fdb2836f6b9154d5057a6ac3727b40
BrianNWZK <brianwaezike@gmail.com>
2025-10-24 03:25:08 +0100

    Update production-omnipotent-bwaezi.js

--- modules/production-omnipotent-bwaezi.js
+++ modules/production-omnipotent-bwaezi.js
@@ -20,11 +20,9 @@ import {
 } from 'crypto';
 import { groth16 } from 'snarkjs';
 import { poseidon } from 'circomlibjs';
-import { 
-  kyber1024, 
-  dilithium5, 
-  falcon1024 
-} from '@openquantumsafe/openssl';
+import { kyber } from 'kyber';
+import { dilithium } from 'pqcrypto';
+import { liboqs } from 'liboqs-node';
 
 // SECURE INFRASTRUCTURE
 import { Worker } from 'worker_threads';
