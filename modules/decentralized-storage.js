6aad1c28d8fa2d42dea111183962f76864b2d51c
BrianNWZK <brianwaezike@gmail.com>
2025-10-24 12:44:44 +0100

    Update decentralized-storage.js

--- modules/decentralized-storage.js
+++ modules/decentralized-storage.js
@@ -1,6 +1,6 @@
 // modules/decentralized-storage.js
 import { EventEmitter } from 'events';
-import { ArielSQLiteEngine } from '../ariel-sqlite-engine/index.js';
+import { ArielSQLiteEngine } from './ariel-sqlite-engine/index.js';
 import { SovereignRevenueEngine } from './sovereign-revenue-engine.js';
 import { 
     BWAEZI_CHAIN,
