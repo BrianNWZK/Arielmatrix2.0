fd4abf371c3bdbc6df965585194ecdb2c25f4d6c
BrianNWZK <brianwaezike@gmail.com>
2025-10-24 14:50:00 +0100

    Update decentralized-exchange.js

--- modules/decentralized-exchange.js
+++ modules/decentralized-exchange.js
@@ -1,7 +1,7 @@
 // modules/decentralized-exchange.js
 import { randomBytes, createHash } from 'crypto';
 import { EventEmitter } from 'events';
-import { ArielSQLiteEngine } from '../ariel-sqlite-engine/index.js';
+import { ArielSQLiteEngine } from './ariel-sqlite-engine/index.js';
 import { SovereignRevenueEngine } from './sovereign-revenue-engine.js';
 import { 
     BWAEZI_CHAIN, 
