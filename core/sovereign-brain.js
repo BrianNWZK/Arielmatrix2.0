94edbfe19076d8395331c3db1420a82e29e1d941
BrianNWZK <brianwaezike@gmail.com>
2025-12-07 14:44:26 +0100

    Update sovereign-brain.js

--- core/sovereign-brain.js
+++ core/sovereign-brain.js
@@ -39,7 +39,7 @@ function getAddressSafely(address) {
 
 const LIVE = {
   VERSION: 'v13.5.5',
-  ENTRY_POINT: getAddressSafely('0x5FF137D4bEAA7036d654a88Ea898df565D304B88'),
+  ENTRY_POINT: getAddressSafely('0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789'),
   ACCOUNT_FACTORY: getAddressSafely('0x9406Cc6185a346906296840746125a0E44976454'),
 
   // Owner + Smart Contract Wallet (SCW)
